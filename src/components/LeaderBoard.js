44f00860a0d0de89e4ad900849c6abe3def9287f
Christopher Stephenson <hpufo2@gmail.com>
2018-08-20 10:27:08 -0400

    have the service sort the topscores instead.

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,14 +3,7 @@ import PropTypes from 'prop-types';
 import styles from '../scss/LeaderBoard.scss';
 
 const renderHighScores = (playerScores) => {
-  //Sort by score and get the top ten players
-  let topPlayers = playerScores.sort((a,b) => {
-    if(a.score > b.score) return -1;
-    else if(a.score < b.score) return 1;
-    return 0;
-  }).slice(0,10);
-
-  return topPlayers.map((player, i) => (
+  return playerScores.map((player, i) => (
     <li className={styles.Row} key={i}>
       <div className={styles.Number}>{i+1})</div>
       <div className={styles.Name}>{player.name}</div>
--- src/service/api.js
+++ src/service/api.js
@@ -4,7 +4,14 @@ const url = 'https://thawing-scrubland-62997.herokuapp.com/api/';
 
 export const getScores = () => {
   return axios.get(`${url}players`)
-    .then(res => res.data)
+    .then(res => {
+      //Return top scores
+      return res.data.sort((a,b) => {
+        if(a.score > b.score) return -1;
+        else if(a.score < b.score) return 1;
+        return 0;
+      }).slice(0,10);
+    })
     .catch(e => {
       throw e;
     });
